faa06ade22a183dfe169b726085ad1d8593afe76
Randall Rodrigues <randallrodrigues@macbizconsulting.net>
2015-06-23 21:49:37 -0400

    removed the custom taxonomy as it was not needed

--- kboom.php
+++ kboom.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: KBoom
-Description: Knowledge Base Taxonomy and Custom Post Type
+Description: Knowledge Base Custom Post Type
 Version: 1.0
 Author: randmanrjr
 Author URI: https://github.com/randmanrjr
@@ -27,44 +27,14 @@ function kboom_post_types() {
             'has_archive'           => true,
             'hierarchical'          => true,
             'show_in_menu'          => true,
-            'rewrite'               => array('slug' => 'kbarticle'),
+            'rewrite'               => array('slug' => 'kbarticles'),
             'supports'              => array('title', 'editor', 'author', 'revisions', 'comments', 'thumbnail', 'excerpt', 'page-attributes'),
-            'taxonomies'            => array('kbarticles')
+            'taxonomies'            => array('post_tag', 'category')
         ));
 }
 
-add_action('init', 'create_kbarticles_taxonomy');
 
-function create_kbarticles_taxonomy() {
-    $labels = array(
-        'name'                          => 'KB Articles',
-        'sigular_name'                  => 'KB Article',
-        'search_items'                  => 'Search Knowledge Base',
-        'all_items'                     => 'All KB Articles',
-        'edit_item'                     => 'Edit KB Article',
-        'update_item'                   => 'Update KB Article',
-        'add_new_item'                  => 'Add New KB Article',
-        'new_item_name'                 => 'New KB Article Name',
-        'menu_name'                     => 'KB Article',
-        'view_item'                     => 'View KB Article',
-        'popular_items'                 => 'Popular KB Articles',
-        'separate_items_with_commas'    => 'Separate KB Articles with commas',
-        'add_or_remove_items'           => 'Add or remove KB Articles',
-        'choose_from_most_used'         => 'Choose from the most accessed KB Aticles',
-        'not_found'                     => 'No matching KB Article'
-    );
-    register_taxonomy(
-        'kbarticles',
-        'kbarticle',
-        array(
-            'label'                     => __('KB Article'),
-            'hierarchical'              => true,
-            'labels'                    => $labels
-        )
-    );
-}
-
-//flush the rewrite rules after theme activation
+//flush the rewrite rules after plugin activation
 
 function kboom_rewrite_flush() {
     kboom_post_types();
